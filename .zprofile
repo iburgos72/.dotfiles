9d0a251917d693faf07ad3f93cf878b53d36110f
Ivan Burgos <ivan.pburgos@gmail.com>
2024-02-19 09:00:41 -0600

    update tmux conf, script and zsh files

---
+++ .zprofile
@@ -0,0 +1,8 @@
+# Added by Toolbox App
+export M2_HOME=/Users/navi/Maven/apache-maven-3.9.6
+JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home"
+export PATH="${M2_HOME}/bin:${JAVA_HOME}/bin:$PATH:/Users/navi/Library/Application Support/JetBrains/Toolbox/scripts"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+
--- master.sh
+++ master.sh
@@ -1,31 +1,57 @@
 #!/bin/bash
 
+echo "------- INSTALLING HOMEBREW -------"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew update && brew upgrade && brew install neovim ripgrep tmux git tig tmux-mem-cpu-load
+echo "------- INSTALLING BREW PACKAGES neovim ripgrep tmux git tig tmux-mem-cpu-load yarn -------"
+brew update && brew upgrade && brew install neovim ripgrep tmux git tig tmux-mem-cpu-load yarn
 
+echo "------- INSTALLING TPM (Tmux Plugin Manager) -------"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+echo "------- DOWNLOADING .DOTFILES -------"
 git clone https://github.com/iburgos72/.dotfiles.git ~/Documents/.dotfiles
 
+echo "------- MOVING .DOTFILES -------"
 cp -r ~/Documents/.dotfiles/nvim ~/.config
 mkdir ~/.config/tmux/
 cp ~/Documents/.dotfiles/.tmux.conf ~/
 cp ~/Documents/.dotfiles/.p10k.zsh ~/
+cp ~/Documents/.dotfiles/.zshrc ~/
+cp ~/Documents/.dotfiles/.zprofile ~/
 
+echo "------- INSTALLING PACKER.NVIM -------"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+echo "------- INSTALLING NVM -------"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
+echo "------- INSTALLING NODE -------"
 nvm install node
 
+echo "------- INSTALLING OH MY ZSH -------"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "------- INSTALLING POWERLEVEL10K -------"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# To download and install
+# - Go 
+# - Iterm2
+# - Insomnia
+# - Docker
+# - DBeaver
+# - Chrome
+# - 1Password
+#
+# Verify node and go are installed or Mason won't install the language servers
+#
 # Once installed run
 # $ nvim ~/.config/nvim/lua/navi/packer
 #
@@ -35,15 +61,7 @@ nvm install node
 # - :PackerSync
 # - :lua Colors()
 #
-# To download and install
-# - Go 
-# - Iterm
-# - Postman
-# - docker
-# - DBeaver
-# - Chrome
-# - 1Password
-#
-# - configure iterm to have transparency
+# - configure iterm to have transparency and disable the keyboard shortcuts ^space in keyboard setings
 # - toggle off "System Settings > Desktop & Dock > Close windows when quitting an application"
 # - un-check "System Settings > Keyboards > Keyboard Shortcuts (button) > Input sources > disable the options"
+# - only if you install java, uncomment the line in .zprofiles
